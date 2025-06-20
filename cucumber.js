d254ab37ca919fa4ebf7c698dcf9c7167e2bdd9c
EmbeddedMhawar <abderrahmanemhawar@gmail.com>
2025-06-20 14:58:25 +0100

    Add cucumber.js to fix the steps definition

---
+++ cucumber.js
@@ -0,0 +1,19 @@
+// cucumber.js
+module.exports = {
+  default: {
+    // Tell Cucumber to use ts-node to execute your .ts files
+    // This MUST be an array.
+    requireModule: ['ts-node/register'],
+
+    // Tell Cucumber where to find your step definition files
+    // This path should match your project structure.
+    require: ['features/**/*.steps.ts'],
+
+    // Optional: Configure reporters for better output
+    format: [
+      'summary',
+      'progress-bar',
+    ],
+    publishQuiet: true,
+  },
+};
